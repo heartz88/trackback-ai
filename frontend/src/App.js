cbc78c435237669d53bb3063319ca9c96d66fed2
DJ <164428859+heartz88@users.noreply.github.com>
2026-03-23 11:43:13 +0000

    button clicking fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,7 @@ function App() {
       <SocketProvider>
         <BrowserRouter>
           <ScrollToTop />
-          <div className="flex flex-col min-h-screen bg-[var(--bg-primary)] transition-colors duration-300">
+          <div className="flex flex-col min-h-screen bg-[var(--bg-primary)]">
             <Navigation />
             <main className="flex-grow pt-20">
               <Routes>
